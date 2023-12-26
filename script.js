2f629224a45e942eb32ff531f11323c2795458c7
Saroj Chaulagain <142041373+SarojChaulagain0@users.noreply.github.com>
2023-12-27 06:22:43 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-// Number of check-in persons based on room type
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//Form Validationn
+function validateAndSubmitForm() {
+  // Customers name
+  var nameRegex = /^[a-zA-Z]+\s[a-zA-Z]+$/;
+  var customerName = document.getElementById('customerName').value;
+  if (!nameRegex.test(customerName)) {
+      alert('Please enter a valid full name (e.g., "John Doe").');
+      return false;
+  }
+
+  // Email address
+  var emailRegex = /\S+@\S+\.\S+/;
+  var email = document.getElementById('email').value;
+  if (!emailRegex.test(email)) {
+      alert('Please enter a valid email address.');
+      return false;
+  }
+
+  // Phone Number
+  var phoneRegex = /^04\d{8}$/;
+  var phone = document.getElementById('phone').value;
+  if (!phoneRegex.test(phone)) {
+      alert('Please enter a valid phone number starting with "04".');
+      return false;
+  }
+
+  // Check-in date
+  var checkInDate = document.getElementById('checkInDate').value;
+  if (new Date(checkInDate) < new Date()) {
+      alert('Please select a future check-in date.');
+      return false;
+  }
+
+  // Number of check-in persons based on room type
   var roomType = document.getElementById('roomType').value;
   var numOfPersons = document.getElementById('numOfPersons').value;
 
