131b872463e620017589bf51cd0ddfd749a49bbf
Saroj Chaulagain <142041373+SarojChaulagain0@users.noreply.github.com>
2023-12-27 06:20:35 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-let mybutton = document.getElementById("myBtn");
+// Number of check-in persons based on room type
+  var roomType = document.getElementById('roomType').value;
+  var numOfPersons = document.getElementById('numOfPersons').value;
 
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  if (
+      (roomType === 'standard_twin' || roomType === 'executive_twin') && numOfPersons > 2 ||
+      (roomType === 'superior_suite' || roomType === 'deluxe_suite' || roomType === 'executive_suite') && numOfPersons > 3 ||
+      (roomType === 'presidential_suite' && numOfPersons > 5)
+  ) {
+      alert('Invalid number of persons for the selected room type.');
+      return false;
   }
+
+  // If all validations pass, submit the form
+  return true;
 }
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+
+//Checking availability
+
+function checkAvailability(roomType) {
+  // Make an AJAX request to check availability
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'checkAvailability.php?roomType=' + roomType, true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        //availability status
+        var response = xhr.responseText;
+        if (response === 'available') {
+          alert('The room is available for booking!');
+        } else if (response === 'booked') {
+          alert('Sorry, the rooms for this type are booked. Please try another room.');
+        } else {
+          alert(response);
+        }
+      } else {
+        //In case ther server request fails
+        alert('Error checking availability. Status code: ' + xhr.status);
+      }
+    }
+  };
+
+  xhr.send();
 }
